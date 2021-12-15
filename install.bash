a9fbf8109bf0b63b756ad38874650854a329b514
Ar-Ray-code <ray255ar@gmail.com>
2021-12-15 21:20:13 +0900

    add install script

--- install.bash
+++ install.bash
@@ -4,13 +4,13 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 if [ "uninstall" = $1 ]; then
     echo "uninstall ..."
     ## REMOVE FILE
-    rm -rf /usr/local/bin/select-file-folder-dialog.py
+    rm -rf /usr/local/bin/select-file-folder-dialog
     exit 0
 
 ## INSTALL =============================================
 
 elif [ "install" = $1 ]; then
-    cp -r $SCRIPT_DIR/select-file-folder-dialog.py /usr/local/bin/
+    cp -r $SCRIPT_DIR/select-file-folder-dialog.py /usr/local/bin/select-file-folder-dialog
 else
     ## ERROR MESSAGE
     echo "options failed ('sudo bash ./install.bash install' or 'sudo bash ./install.bash uninstall')"
